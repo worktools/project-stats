1005c5d3081a1255fdcfc6984b9a42828a16099a
ChenYong <chenyong@jimeng.io>
2019-05-28 11:19:25 +0800

    making use of chan-utils

--- src/app/collector.cljs
+++ src/app/collector.cljs
@@ -2,32 +2,35 @@
 (ns app.collector
   (:require [cljs-node-io.fs :refer [areaddir areadFile awriteFile adir? astat areadFile]]
             [clojure.core.async :refer [go chan >! <!]]
-            [cumulo-util.async :refer [all-once]]
             ["path" :as path]
             [favored-edn.core :refer [write-edn]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [chan-utils.core :refer [chan-once all-once]]))
 
 (defn chan-gather-entries [base-dir entries]
   (->> entries
        (filter
         (fn [entry]
-          (not (contains? #{".cache-loader" ".awcache" ".hswcache" "dist" "dll"} entry))))
+          (not
+           (contains?
+            #{".cache-loader" ".awcache" ".hswcache" "dist" "dll" ".sass-cache" "_scss"}
+            entry))))
        (all-once
         (fn [entry]
-          (let [<child-result (chan)]
-            (go
-             (let [child (path/join base-dir entry), is-dir? (<! (adir? child))]
-               (if is-dir?
-                 (let [[err children] (<! (areaddir child))
-                       results (<! (chan-gather-entries child children))]
-                   (>! <child-result {:name entry, :path child, :children results}))
-                 (let [[err stat] (<! (astat child))
-                       [err content] (<! (areadFile child "utf8"))
-                       lines-count (count (string/split content "\n"))]
-                   (>!
-                    <child-result
-                    {:name entry, :path child, :size (.-size stat), :lines lines-count})))))
-            <child-result)))))
+          (chan-once
+           got
+           (go
+            (let [child (path/join base-dir entry), is-dir? (<! (adir? child))]
+              (if is-dir?
+                (let [[err children] (<! (areaddir child))
+                      results (<! (chan-gather-entries child children))]
+                  (got {:name entry, :path child, :children results}))
+                (let [[err stat] (<! (astat child))
+                      [err content] (<! (areadFile child "utf8"))
+                      lines-count (->> (string/split content "\n")
+                                       (filter (fn [x] (not (string/blank? x))))
+                                       (count))]
+                  (got {:name entry, :path child, :size (.-size stat), :lines lines-count}))))))))))
 
 (def picked-entries ["shared" "account" "system" "admin" "fi" "global"])
 
